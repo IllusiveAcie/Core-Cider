b65f276713f449d2e64908340d1afaa4f681303b
IllusiveAcie <yxshhp@gmail.com>
2026-08-22 12:55:12 +0300

    game rules

--- src/main/java/net/lxshh/cider/events/CiderEventHandler.java
+++ src/main/java/net/lxshh/cider/events/CiderEventHandler.java
@@ -6,11 +6,14 @@ import net.dries007.tfc.common.player.IPlayerInfo;
 import net.lxshh.cider.Cider;
 import net.lxshh.cider.util.CiderHelpers;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.ExperienceOrb;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodData;
+import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.bus.api.EventPriority;
@@ -21,11 +24,27 @@ import net.neoforged.neoforge.event.entity.living.LivingExperienceDropEvent;
 import net.neoforged.neoforge.event.entity.living.LivingShieldBlockEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import net.neoforged.neoforge.event.entity.player.UseItemOnBlockEvent;
+import net.neoforged.neoforge.event.level.LevelEvent;
 
 import java.util.Objects;
 
 @EventBusSubscriber(modid = Cider.MOD_ID)
 public class CiderEventHandler {
+
+    @SubscribeEvent
+    public static void onWorldLoad(LevelEvent.Load event) {
+        if (event.getLevel() instanceof ServerLevel level) {
+            final MinecraftServer server = level.getServer();
+            final GameRules rules = level.getGameRules();
+
+            rules.getRule(GameRules.RULE_NATURAL_REGENERATION).set(false, server);
+            rules.getRule(GameRules.RULE_DOINSOMNIA).set(false, server);
+            rules.getRule(GameRules.RULE_DO_PATROL_SPAWNING).set(false, server);
+            rules.getRule(GameRules.RULE_DO_TRADER_SPAWNING).set(false, server);
+            rules.getRule(GameRules.RULE_DO_WARDEN_SPAWNING).set(false, server);
+            rules.getRule(GameRules.RULE_BLOCK_EXPLOSION_DROP_DECAY).set(false, server);
+        }
+    }
 
     @SubscribeEvent
     public static void onUseItemBlockEvent(UseItemOnBlockEvent event) {
